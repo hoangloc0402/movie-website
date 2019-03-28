cdea2cd23c3fabbfa4134e0d071c986684993070
tienbk97 <nghuuanhtien@gmail.com>
2019-03-28 09:19:45 +0700

    tvshow page

--- tvshowpage/tvshowpage.js
+++ tvshowpage/tvshowpage.js
@@ -1,159 +1,161 @@
-const movie_name = "Grey's Anatomy - Season 8"
-const src = "images/1.png";
-const released_date = "2011";
-const imdb = 7.7;
-const tag = [
-	{g:"Drama",s:"#"},
-	{g:"Romance",s:"#"}
-]
-const description = "In this season, challenges keep coming. Loyalties will be tested, relationships will be strained and everyone’s future will hang in the balance.But no matter how hard life is, the doctors of Seattle Grace know they can always lean on one another."
-const list_ep = [
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 1
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 2
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 3
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 4
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 5
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 6
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 7
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 8
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 9
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 10
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 11
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 12
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 13
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 14
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 15
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 18,
-		ep: 16
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 17
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 18
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 19
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 20
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 21
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 22
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 23
-	},
-	{
-		href : "#",
-		name : "Flight",
-		season: 8,
-		ep: 24
-	},
-
-];
+const movie = {
+	movie_name : "Grey's Anatomy - Season 8",
+	src : "images/1.png",
+	released_date : "2011",
+	imdb : 7.7,
+	tag : [
+		{g:"Drama",s:"#"},
+		{g:"Romance",s:"#"}
+	],
+	description : "In this season, challenges keep coming. Loyalties will be tested, relationships will be strained and everyone’s future will hang in the balance.But no matter how hard life is, the doctors of Seattle Grace know they can always lean on one another.",
+	list_ep : [
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 1
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 2
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 3
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 4
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 5
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 6
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 7
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 8
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 9
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 10
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 11
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 12
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 13
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 14
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 15
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 18,
+			ep: 16
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 17
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 18
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 19
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 20
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 21
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 22
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 23
+		},
+		{
+			href : "#",
+			name : "Flight",
+			season: 8,
+			ep: 24
+		},
+	
+	]
+}
 
 
 var img_movie_rendered = false;
@@ -200,15 +202,15 @@ var img_movie_rendered = false;
 		var card_width = gallery_width / 2 - 2 * gallery_margin;
 		console.log("HERE", gallery_width, 2, card_width, img_movie_rendered );
 		if (img_movie_rendered === false) {
-			for (var i = list_ep.length - 1; i >= list_ep.length - 2; i --) {
+			for (var i = movie.list_ep.length - 1; i >= movie.list_ep.length - 2; i --) {
 				var div =  $(`<div class="movie-thumnail"></div>`)
-				div.css('background-image', 'url(' + src + ')');
+				div.css('background-image', 'url(' + movie.src + ')');
 				div.append(`<span class="movie-play-button"></span>`)
 				$(div).width(card_width);
 
 				var wrapper = $(`<div class="movie-thumnail-wrapper"></div>`)
 				wrapper.append(div,
-					`<span class="movie-title">${movie_name + " Episode " + list_ep[i].ep + ": " + list_ep[i].name}</span>`)
+					`<span class="movie-title">${movie.movie_name + " Episode " + movie.list_ep[i].ep + ": " + movie.list_ep[i].name}</span>`)
 				$(wrapper).width(card_width);
 				$("#latest_ep_thumnail").append(wrapper);
 			}
@@ -226,15 +228,15 @@ var img_movie_rendered = false;
 		console.log("HERE", gallery_width, gallery_col, card_width, img_movie_rendered );
 		if (img_movie_rendered === false) {
 			img_movie_rendered = true;
-			for (var i = list_ep.length - 1; i >= 0; i --) {
+			for (var i = movie.list_ep.length - 1; i >= 0; i --) {
 				var div =  $(`<div class="movie-thumnail"></div>`)
-				div.css('background-image', 'url(' + src + ')');
+				div.css('background-image', 'url(' + movie.src + ')');
 				div.append(`<span class="movie-play-button"></span>`)
 				$(div).width(card_width);
 
 				var wrapper = $(`<div class="movie-thumnail-wrapper"></div>`)
 				wrapper.append(div,
-					`<span class="movie-title">${movie_name + " Episode " + list_ep[i].ep + ": " + list_ep[i].name}</span>`)
+					`<span class="movie-title">${movie.movie_name + " Episode " + movie.list_ep[i].ep + ": " + movie.list_ep[i].name}</span>`)
 				$(wrapper).width(card_width);
 				$("#gallery").append(wrapper);
 			}
@@ -253,17 +255,18 @@ var img_movie_rendered = false;
     // 
     // Execute only after document has fully loaded
     $(document).ready(function() {
-			$("#movie-name").text(movie_name);
-			$("#movies_name_div").text(movie_name);
-			$("#movies_poster").attr("src",src);
-			$("#movies_released_date").append(released_date);
-			$("#movies_imdb").append(imdb);
-			$("#movies_desp").text(description);
-			for (var i = 0; i < tag.length; i++) {
+			$("#movie-name").text(movie.movie_name);
+			$("#movies_name_div").text(movie.movie_name);
+			$("#movies_name_gallery").text(movie.movie_name + " - Episodes List");
+			$("#movies_poster").attr("src",movie.src);
+			$("#movies_released_date").append(movie.released_date);
+			$("#movies_imdb").append(movie.imdb);
+			$("#movies_desp").text(movie.description);
+			for (var i = 0; i < movie.tag.length; i++) {
 				if (i == 0)
-					$("#movies_tags").append(`<a href=${tag[i].s}>${tag[i].g}</a>`)
+					$("#movies_tags").append(`<a href=${movie.tag[i].s}>${movie.tag[i].g}</a>`)
 				else 
-					$("#movies_tags").append(` / <a href=${tag[i].s}> ${tag[i].g}</a>`)
+					$("#movies_tags").append(` / <a href=${movie.tag[i].s}> ${movie.tag[i].g}</a>`)
 			}
 			render_slide();
 			// console.log($('#gallery'))
