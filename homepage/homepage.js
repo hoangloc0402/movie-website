e2e012dd26eb7a0fedcae7578a7257996dab4a1e
tienbk97 <nghuuanhtien@gmail.com>
2019-03-28 12:58:44 +0700

    collapse

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,214 +1,214 @@
 const list_top_movies = [
 	{
-		src : "images/1.png",
-		name : "Alia: Battle Angle", "href" : "#", "ep" : "Episode 01"
+		src: "images/1.png",
+		name: "Alia: Battle Angle", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/2.png",
-		name : "A Madea Family Funeral", "href" : "#", "ep" : "Episode 01"
+		src: "images/2.png",
+		name: "A Madea Family Funeral", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/3.png",
-		name : "Aquaman", "href" : "#", "ep" : "Episode 01"
+		src: "images/3.png",
+		name: "Aquaman", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/4.png",
-		name : "Bumblebee", "href" : "#", "ep" : "Episode 01"
+		src: "images/4.png",
+		name: "Bumblebee", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/5.png",
-		name : "Captain Marvel", "href" : "#", "ep" : "Episode 01"
+		src: "images/5.png",
+		name: "Captain Marvel", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/6.png",
-		name : "Captive State", "href" : "#", "ep" : "Episode 01"
+		src: "images/6.png",
+		name: "Captive State", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/7.png",
-		name : "Dragged Across Concrete", "href" : "#", "ep" : "Episode 01"
+		src: "images/7.png",
+		name: "Dragged Across Concrete", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/8.png",
-		name : "Fantastic Beasts: The Crimes of Grindelwald", "href" : "#", "ep" : "Episode 01"
+		src: "images/8.png",
+		name: "Fantastic Beasts: The Crimes of Grindelwald", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/9.png",
-		name : "Holmes and Watson", "href" : "#", "ep" : "Episode 01"
+		src: "images/9.png",
+		name: "Holmes and Watson", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/10.png",
-		name : "How to Train Your Dragon: The Hidden World", "href" : "#", "ep" : "Episode 01"
+		src: "images/10.png",
+		name: "How to Train Your Dragon: The Hidden World", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/11.png",
-		name : "Leaving Neverland", "href" : "#", "ep" : "Episode 01"
+		src: "images/11.png",
+		name: "Leaving Neverland", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/12.png",
-		name : "Mary Poppins Returns", "href" : "#", "ep" : "Episode 01"
+		src: "images/12.png",
+		name: "Mary Poppins Returns", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/13.png",
-		name : "Marriage", "href" : "#", "ep" : "Episode 01"
+		src: "images/13.png",
+		name: "Marriage", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/14.png",
-		name : "The Hard Way", "href" : "#", "ep" : "Episode 01"
+		src: "images/14.png",
+		name: "The Hard Way", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/15.png",
-		name : "The Kid Who Would Be King", "href" : "#", "ep" : "Episode 01"
+		src: "images/15.png",
+		name: "The Kid Who Would Be King", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/16.png",
-		name : "The Lego Movie 2: The Second Part The Lego Movie 2: The Second Part", "href" : "#", "ep" : "Episode 01"
+		src: "images/16.png",
+		name: "The Lego Movie 2: The Second Part The Lego Movie 2: The Second Part", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/17.png",
-		name : "The Mule", "href" : "#", "ep" : "Episode 01"
+		src: "images/17.png",
+		name: "The Mule", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/18.png",
-		name : "Triple Frontier", "href" : "#", "ep" : "Episode 01"
+		src: "images/18.png",
+		name: "Triple Frontier", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/19.png",
-		name : "Us", "href" : "#", "ep" : "Episode 01"
+		src: "images/19.png",
+		name: "Us", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/20.png",
-		name : "Walk. Ride. Rodeo.", "href" : "#", "ep" : "Episode 01"
+		src: "images/20.png",
+		name: "Walk. Ride. Rodeo.", "href": "#", "ep": "Episode 01"
 	}
 ];
 
 const list_movies = [
 	{
-		src : "images/0_1.png",
-		name : "Those Who Can't - Season 1", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_1.png",
+		name: "Those Who Can't - Season 1", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_2.png",
-		name : "Absentia - Season 2", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_2.png",
+		name: "Absentia - Season 2", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_3.png",
-		name : "American Housewife - Season 4", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_3.png",
+		name: "American Housewife - Season 4", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_4.png",
-		name : "Bar Rescue - Season 3", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_4.png",
+		name: "Bar Rescue - Season 3", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_5.png",
-		name : "Dateline: Secrets Uncovered", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_5.png",
+		name: "Dateline: Secrets Uncovered", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_6.png",
-		name : "The Tonight Show Starring Jimmy Fallon", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_6.png",
+		name: "The Tonight Show Starring Jimmy Fallon", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_7.png",
-		name : "Deadliest Catch", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_7.png",
+		name: "Deadliest Catch", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_8.png",
-		name : "Deadliest Catch", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_8.png",
+		name: "Deadliest Catch", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_9.png",
-		name : "Derry Girls", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_9.png",
+		name: "Derry Girls", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_10.png",
-		name : "The Great British Sewing Bee", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_10.png",
+		name: "The Great British Sewing Bee", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_11.png",
-		name : "East Enders", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_11.png",
+		name: "East Enders", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_12.png",
-		name : "Law & Order", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_12.png",
+		name: "Law & Order", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_13.png",
-		name : "Five Feet Apart", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_13.png",
+		name: "Five Feet Apart", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_14.png",
-		name : "Go!", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_14.png",
+		name: "Go!", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_15.png",
-		name : "Good Trouble", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_15.png",
+		name: "Good Trouble", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_16.png",
-		name : "Haunted Case Files", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_16.png",
+		name: "Haunted Case Files", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_17.png",
-		name : "The Rookie", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_17.png",
+		name: "The Rookie", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_18.png",
-		name : "Holby City", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_18.png",
+		name: "Holby City", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_19.png",
-		name : "Indecent", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_19.png",
+		name: "Indecent", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_20.png",
-		name : "Ice Cold Blood", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_20.png",
+		name: "Ice Cold Blood", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_21.png",
-		name : "Kim's Convenience", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_21.png",
+		name: "Kim's Convenience", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_22.png",
-		name : "The Late Show With Stephen Colbert", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_22.png",
+		name: "The Late Show With Stephen Colbert", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_23.png",
-		name : "MasterChef: Kids", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_23.png",
+		name: "MasterChef: Kids", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_24.png",
-		name : "MasterChef", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_24.png",
+		name: "MasterChef", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_25.png",
-		name : "PC game", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_25.png",
+		name: "PC game", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_26.png",
-		name : "Muiafukaz", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_26.png",
+		name: "Muiafukaz", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_27.png",
-		name : "NCIS", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_27.png",
+		name: "NCIS", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_28.png",
-		name : "Schitt$ Creek", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_28.png",
+		name: "Schitt$ Creek", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_29.png",
-		name : "THe Story Of God", "href" : "#", "ep" : "Episode 01"
+		src: "images/0_29.png",
+		name: "THe Story Of God", "href": "#", "ep": "Episode 01"
 	},
 	{
-		src : "images/0_30.png",
-		name : "The Village", "href" : "#", "ep" : "Episode 01 Episode 01 Episode 01"
+		src: "images/0_30.png",
+		name: "The Village", "href": "#", "ep": "Episode 01 Episode 01 Episode 01"
 	}
 ];
 
 
 var img_movie_rendered = false;
 
-(function($, viewport){
-    // Bootstrap 4 Divs
+(function ($, viewport) {
+	// Bootstrap 4 Divs
 	var bootstrapDivs = {
 		'xs': $('<div class="device-xs d-block d-sm-none"></div>'),
 		'sm': $('<div class="device-sm d-none d-sm-block d-md-none"></div>'),
@@ -226,15 +226,18 @@ var img_movie_rendered = false;
 	var gallery_margin = 10;
 	function render_slide() {
 		var op = false;
-		if( viewport.is('xs') ) {
+		if (viewport.is('xs')) {
 			gallery_col = 2;
 			gallery_margin = 5;
-			rows = 1;
-			col_per_row = 2;
-			pages = Math.floor(total_movies / (rows * col_per_row));
+			if ($("#bd-docs-nav").hasClass("show") === true) $("#bd-docs-nav").removeClass("show");
+			// rows = 1;
+			// col_per_row = 2;
+			// pages = Math.floor(total_movies / (rows * col_per_row));
+			pages = 0;
 			console.log("xs:", pages, rows, col_per_row)
 		}
-		if( viewport.is('>=sm') ) {
+		if (viewport.is('>=sm')) {
+			if ($("#bd-docs-nav").hasClass("show") === true) $("#bd-docs-nav").removeClass("show");
 			gallery_col = 2;
 			gallery_margin = 5;
 			rows = 1;
@@ -242,7 +245,8 @@ var img_movie_rendered = false;
 			pages = Math.floor(total_movies / (rows * col_per_row));
 			console.log("sm:", pages, rows, col_per_row)
 		}
-		if( viewport.is('>=md') ) {
+		if (viewport.is('>=md')) {
+			if ($("#bd-docs-nav").hasClass("show") === false) $("#bd-docs-nav").addClass("show");
 			gallery_col = 3;
 			gallery_margin = 10;
 			rows = 2;
@@ -250,7 +254,8 @@ var img_movie_rendered = false;
 			pages = Math.floor(total_movies / (rows * col_per_row));
 			console.log("md:", pages, rows, col_per_row)
 		}
-		if( viewport.is('>=lg') ) {
+		if (viewport.is('>=lg')) {
+			if ($("#bd-docs-nav").hasClass("show") === false) $("#bd-docs-nav").addClass("show");
 			gallery_col = 4;
 			gallery_margin = 10;
 			rows = 2;
@@ -259,7 +264,8 @@ var img_movie_rendered = false;
 			console.log("lg:", pages, rows, col_per_row)
 		}
 
-		if( viewport.is('>=xl') ) {
+		if (viewport.is('>=xl')) {
+			if ($("#bd-docs-nav").hasClass("show") === false) $("#bd-docs-nav").addClass("show");
 			gallery_col = 5;
 			gallery_margin = 10;
 			op = true;
@@ -268,8 +274,8 @@ var img_movie_rendered = false;
 			pages = Math.floor(total_movies / (rows * col_per_row));
 			console.log("xl:", pages, rows, col_per_row)
 		}
-		var width = $(window).innerWidth(); 
-		var height = $( window ).height();
+		var width = $(window).innerWidth();
+		var height = $(window).height();
 		document.documentElement.clientHeight;
 
 		const height_per_row = height / 5;
@@ -304,7 +310,6 @@ var img_movie_rendered = false;
 					counter++;
 				}
 
-				//console.log(row.html());
 				carousel_item.append(row);
 			}
 
@@ -314,14 +319,14 @@ var img_movie_rendered = false;
 		}
 
 		var gallery_width = $("#movies_content").width();
-		
+
 		var card_width = gallery_width / gallery_col - 2 * gallery_margin;
-		console.log("HERE", gallery_width, gallery_col, card_width, img_movie_rendered );
+		console.log("HERE", gallery_width, gallery_col, card_width, img_movie_rendered);
 		if (img_movie_rendered === false) {
 			img_movie_rendered = true;
-			for (var i = 0; i < list_movies.length; i ++) {
-				
-				var div =  $(`<div class="movie-thumnail"></div>`)
+			for (var i = 0; i < list_movies.length; i++) {
+
+				var div = $(`<div class="movie-thumnail"></div>`)
 				div.css('background-image', 'url(' + list_movies[i].src + ')');
 				div.append(
 					`<span class="movie-title">${list_movies[i].name}</span>`,
@@ -333,26 +338,25 @@ var img_movie_rendered = false;
 			}
 		} else {
 			var arr = $("#gallery").find("div");
-			for (var i = 0; i < arr.length; i ++) {
+			for (var i = 0; i < arr.length; i++) {
 				$(arr[i]).width(card_width)
 			}
 		}
 
-    }
+	}
 
-    // 
-    // Execute only after document has fully loaded
-    $(document).ready(function() {
+	// 
+	// Execute only after document has fully loaded
+	$(document).ready(function () {
 		render_slide();
-		// console.log($('#gallery'))
-    });
+	});
 
-    // Execute code each time window size changes
-    $(window).resize(
-		viewport.changed(function() {
-		$("#homepage_popular_movies_inner").html("")
-		render_slide();
+	// Execute code each time window size changes
+	$(window).resize(
+		viewport.changed(function () {
+			$("#homepage_popular_movies_inner").html("")
+			render_slide();
 		})
-    ); 
+	);
 
 })(jQuery, ResponsiveBootstrapToolkit);
