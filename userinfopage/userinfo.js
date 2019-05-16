6817aaf19386b0ad9e721f13bb2ee6fff852382e
lampard <thanh230497@gmail.com>
2019-05-16 12:12:48 +0700

    Complete initial loading user info page

--- userinfopage/userinfo.js
+++ userinfopage/userinfo.js
@@ -12,3 +12,22 @@ function fasterPreview(uploader) {
 $("#imageUpload").change(function(){
     fasterPreview(this);
 });
+
+$(document).ready(function(){
+    $.ajax({
+        type: 'GET',
+        url: '../api/user.php',
+        dataType: "json",
+        async:true,
+        data: {
+            user_id: 1
+        },
+        success: function(data) {
+            console.log(data['data'])
+            $("#display_name").val(data['data']['user_name'])
+            $("#email").val(data['data']['user_email'])
+            $("#profileImage").attr('src', data['data']['user_profile_image'])
+        }
+
+    });
+});
