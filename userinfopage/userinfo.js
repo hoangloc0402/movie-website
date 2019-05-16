1daa6c6393bb636c4c7443d23b0178ec23b9d838
lampard <thanh230497@gmail.com>
2019-05-16 10:47:44 +0700

    Update userinfo page

--- userinfopage/userinfo.js
+++ userinfopage/userinfo.js
@@ -1,65 +1,14 @@
-(function ($, viewport) {
-	// Bootstrap 4 Divs
-	var bootstrapDivs = {
-		'xs': $('<div class="device-xs d-block d-sm-none"></div>'),
-		'sm': $('<div class="device-sm d-none d-sm-block d-md-none"></div>'),
-		'md': $('<div class="device-md d-none d-md-block d-lg-none"></div>'),
-		'lg': $('<div class="device-lg d-none d-lg-block d-xl-none"></div>'),
-		'xl': $('<div class="device-xl d-none d-xl-block butts"></div>')
-	};
-    viewport.use('bs4', bootstrapDivs);
-    
-	function render_slide() {
-		var op = false;
-		if (viewport.is('xs')) {
-			if ($("#bd-docs-nav").hasClass("show") === true) $("#bd-docs-nav").removeClass("show");
-		}
-		if (viewport.is('>=sm')) {
-			if ($("#bd-docs-nav").hasClass("show") === true) $("#bd-docs-nav").removeClass("show");
-		}
-		if (viewport.is('>=md')) {
-			if ($("#bd-docs-nav").hasClass("show") === false) $("#bd-docs-nav").addClass("show");
-		}
-		if (viewport.is('>=lg')) {
-			if ($("#bd-docs-nav").hasClass("show") === false) $("#bd-docs-nav").addClass("show");
-		}
+$("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+});
 
-		if (viewport.is('>=xl')) {
-			if ($("#bd-docs-nav").hasClass("show") === false) $("#bd-docs-nav").addClass("show");
-		}
-	}
-	$(document).ready(function () {
-		render_slide();
-	});
-	$(window).resize(
-		viewport.changed(function () {
-			render_slide();
-		})
-	);
-
-})(jQuery, ResponsiveBootstrapToolkit);
-
-$(document).ready(function() {
-
-    
-    var readURL = function(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
-            }
-    
-            reader.readAsDataURL(input.files[0]);
-        }
+function fasterPreview(uploader) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#profileImage').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]));
     }
-    
+}
 
-    $(".file-upload").on('change', function(){
-        readURL(this);
-    });
-    
-    $(".upload-button").on('click', function() {
-       $(".file-upload").click();
-    });
+$("#imageUpload").change(function(){
+    fasterPreview(this);
 });
